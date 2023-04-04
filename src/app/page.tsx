e318a79639f26eb4056e288773fc66cc5a7064b7
Daniel Junior <oidanieljunior@gmail.com>
2023-04-03 22:36:44 -0300

    setup tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex h-full items-center justify-center bg-brand">
       <h2>heading</h2>
     </main>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      brand: '#1da1f2',
+      white: '#fff',
+      black: '#000',
+      'gray-light': '#6e767d',
+      'gray-medium': '#2f3336',
+      'gray-dark': '#15181c'
+    }
+  },
+  plugins: []
+};
