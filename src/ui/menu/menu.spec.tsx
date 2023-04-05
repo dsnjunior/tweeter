8a47c5d5b0df16f7241e9d6e27a66471f32e77f6
Daniel Junior <oidanieljunior@gmail.com>
2023-04-05 20:19:15 -0300

    feat: create menu component
    ci: update ci steps names

---
+++ src/ui/menu/menu.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen, axe } from 'testing/unit';
+
+import { Menu } from '.';
+
+describe('<Menu />', () => {
+  it('renders the menu', () => {
+    render(<Menu />);
+
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(8);
+    expect(screen.getAllByRole('link')).toHaveLength(9);
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Explore')).toBeInTheDocument();
+    expect(screen.getByText('Notifications')).toBeInTheDocument();
+    expect(screen.getByText('Messages')).toBeInTheDocument();
+    expect(screen.getByText('Bookmarks')).toBeInTheDocument();
+    expect(screen.getByText('Lists')).toBeInTheDocument();
+    expect(screen.getByText('Profile')).toBeInTheDocument();
+    expect(screen.getByText('More')).toBeInTheDocument();
+    expect(screen.getByText('Tweet')).toBeInTheDocument();
+  });
+
+  describe('a11y', () => {
+    it('has no a11y violations', async () => {
+      const { container } = render(<Menu />);
+
+      expect(await axe(container)).toHaveNoViolations();
+    });
+  });
+});
---
+++ src/ui/menu/menu.tsx
@@ -0,0 +1,81 @@
+import Link from 'next/link';
+import { Button } from '../button';
+import {
+  BrandIcon,
+  HomeIcon,
+  ExploreIcon,
+  NotificationsIcon,
+  MessagesIcon,
+  BookmarksIcon,
+  ListsIcon,
+  ProfileIcon,
+  MoreIcon
+} from './icons';
+
+const items = [
+  {
+    label: 'Home',
+    href: '/',
+    icon: <HomeIcon />
+  },
+  {
+    label: 'Explore',
+    href: '/explore',
+    icon: <ExploreIcon />
+  },
+  {
+    label: 'Notifications',
+    href: '/notifications',
+    icon: <NotificationsIcon />
+  },
+  {
+    label: 'Messages',
+    href: '/messages',
+    icon: <MessagesIcon />
+  },
+  {
+    label: 'Bookmarks',
+    href: '/bookmarks',
+    icon: <BookmarksIcon />
+  },
+  {
+    label: 'Lists',
+    href: '/lists',
+    icon: <ListsIcon />
+  },
+  {
+    label: 'Profile',
+    href: '/profile',
+    icon: <ProfileIcon />
+  },
+  {
+    label: 'More',
+    href: '/more',
+    icon: <MoreIcon />
+  }
+] as const;
+
+export function Menu() {
+  return (
+    <div className="bg-black text-white">
+      <nav>
+        <Link href="/" aria-label="Tweeter" className="block w-fit p-4 leading-none hover:text-brand">
+          <BrandIcon />
+        </Link>
+        <ul>
+          {items.map(({ href, icon, label }) => (
+            <li key={href}>
+              <Link href={href} className="flex items-center p-4 hover:text-brand">
+                <span>{icon}</span>
+                <span className="ml-2 text-sm font-semibold">{label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="mt-4">
+        <Button label="Tweet" size="large" fullWidth />
+      </div>
+    </div>
+  );
+}
