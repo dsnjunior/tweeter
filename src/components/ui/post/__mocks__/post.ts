7f185d9ec331423f5b678b94104ec22ba2af8651
Daniel Junior <oidanieljunior@gmail.com>
2023-06-25 21:48:09 -0300

    refactor: move ui components to components dir
    Change how post form component is made

--- src/components/ui/post/__mocks__/post.ts
+++ src/components/ui/post/__mocks__/post.ts
@@ -1,86 +1,92 @@
 import { PostProps } from "..";
 
-import mockedImage from './image.png'
+import mockedImage from "./image.png";
 
 export const mockedPostWithoutImage: PostProps = {
   data: {
     author: {
-      name: 'Leo',
-      username: '@leo',
+      name: "Leo",
+      username: "@leo",
       avatar: {
         src: mockedImage,
-        alt: 'Leo`s avatar',
+        alt: "Leo`s avatar",
       },
     },
-    timeAgo: '10 min',
-    content: 'Life Update: I’m joining CR Vasco da Gama today. Honored to have worked with amazing people at Sao Paulo FC. Hope I don’t go back. Yay!',
+    timeAgo: "10 min",
+    content:
+      "Life Update: I’m joining CR Vasco da Gama today. Honored to have worked with amazing people at Sao Paulo FC. Hope I don’t go back. Yay!",
     images: [],
-    comments: '124',
-    rePosts: '30',
-    likes: '2.2k'
+    comments: "124",
+    rePosts: "30",
+    likes: "2.2k",
   },
-
-  onComment: () => null,
-  onRePost: () => null,
-  onLike: () => null,
-  onShare: () => null,
-}
+};
 
 export const mockedPostWithOneImage: PostProps = {
   ...mockedPostWithoutImage,
   data: {
     ...mockedPostWithoutImage.data,
-    images: [{
-      src: mockedImage,
-      alt: 'mock image',
-    }]
+    images: [
+      {
+        src: mockedImage,
+        alt: "mock image",
+      },
+    ],
   },
-}
+};
 
 export const mockedPostWithTwoImages: PostProps = {
   ...mockedPostWithoutImage,
   data: {
     ...mockedPostWithoutImage.data,
     images: [
-      ...mockedPostWithOneImage.data.images, {
+      ...mockedPostWithOneImage.data.images,
+      {
         src: mockedImage,
-        alt: 'mock image 2',
-      }]
+        alt: "mock image 2",
+      },
+    ],
   },
-}
+};
 
 export const mockedPostWithThreeImages: PostProps = {
   ...mockedPostWithoutImage,
   data: {
     ...mockedPostWithoutImage.data,
     images: [
-      ...mockedPostWithTwoImages.data.images, {
+      ...mockedPostWithTwoImages.data.images,
+      {
         src: mockedImage,
-        alt: 'mock image 3',
-      }]
+        alt: "mock image 3",
+      },
+    ],
   },
-}
+};
 
 export const mockedPostWithFourImages: PostProps = {
   ...mockedPostWithoutImage,
   data: {
     ...mockedPostWithoutImage.data,
     images: [
-      ...mockedPostWithThreeImages.data.images, {
+      ...mockedPostWithThreeImages.data.images,
+      {
         src: mockedImage,
-        alt: 'mock image 4',
-      }]
+        alt: "mock image 4",
+      },
+    ],
   },
-}
+};
 
 export const mockedPostWithFiveImages: PostProps = {
   ...mockedPostWithoutImage,
   data: {
     ...mockedPostWithoutImage.data,
     images: [
-      ...mockedPostWithFourImages.data.images, {
+      ...mockedPostWithFourImages.data.images,
+      {
         src: mockedImage,
-        alt: 'mock image 5',
-      }]
+        alt: "mock image 5",
+      },
+    ],
   },
-}
+};
